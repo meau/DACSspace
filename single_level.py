9f686f3bf80a7bd3561631123e055d3cc2892464
Amy Berish <aberish@rockarch.org>
2016-05-23 14:37:06 -0400

    return content and limit to published records only

--- single_level.py
+++ single_level.py
@@ -36,44 +36,44 @@ def get_values(resource, array, value):
 			value_types.append(item[value])
 		else:
 			value_types.append("false")
-	return value_types
+	return " , ".join(value_types)
 	
 def get_single_value(resource, key):
 	d = resource
 	if key in d:
-		return key
+		return d.get(key)
 	else:
 		return "false"
-		
+	
 def makeRow(resource):
 	global row
 	row = []
 	publish = get_single_value(resource, "publish")
+	title = get_single_value(resource, "title")
+	resourceId = get_single_value(resource, "id_0")
 	extent = get_values(resource, "extents", "number")
 	date = get_values(resource, "dates", "date_type")
 	agent = get_values(resource, "linked_agents", "role")
 	language = get_single_value(resource, "language")
 	repository = get_single_value(resource, "repository")
-	required_values = publish, extent, date, language, repository
+	required_values = title, resourceId, extent, date, language, repository
 	notes_list = get_note_types(resource)
-	row.append(resource["title"].encode("utf-8"))
-	row.append(resource["id_0"])
 	
 	for item in required_values:
-		if item != "false":
-			row.append("true")
+		if item != "false": 
+				row.append(item)
 		else:
-			row.append("false")
+			row.append("false")				
 
 	if "creator" in agent:
-		row.append("true")
+		row.append(agent)
 	else:
 		row.append("false")	
-						
+							
 	for note in required_notes:
 		if notes_list:
 			if note in notes_list:
-				row.append("true")
+				row.append(note)
 			else:
 				row.append("false")
 		else:
@@ -81,19 +81,21 @@ def makeRow(resource):
 	print row
 	return row
 
+
 def main():
 	print "Creating a csv"
 	writer = csv.writer(open(spreadsheet, "w"))
-	column_headings = ["title","resource", "publish", "extent", "date", "language", "repository", "creator"] + required_notes
+	column_headings = ["title", "resource", "extent", "date", "language", "repository", "creator"] + required_notes
 	writer.writerow(column_headings)
 
 	print "Getting a list of resources"
 	resourceIds = requests.get(repositoryBaseURL + "/resources?all_ids=true", headers=headers)
 	print "Writing rows"
 	for resourceId in resourceIds.json():
-		resource = (requests.get(repositoryBaseURL + "/resources/" + str(resourceId), headers=headers)).json()
-		makeRow(resource)
-		writer.writerow(row)
+			resource = (requests.get(repositoryBaseURL + "/resources/" + str(resourceId), headers=headers)).json()
+			if resource["publish"]:
+				makeRow(resource)
+				writer.writerow(row)
 	spreadsheet.close()
 
 main()
